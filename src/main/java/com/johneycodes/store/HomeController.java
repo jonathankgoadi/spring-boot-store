e805140588cc38dc6afcd730c55a7a94b56e360d
Jonathan Kgoadi <jonathankgoadi@gmail.com>
2025-06-19 15:16:44 +0200

    init project

---
+++ src/main/java/com/johneycodes/store/HomeController.java
@@ -0,0 +1,17 @@
+package com.johneycodes.store;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @RequestMapping("/")
+    public  String index(){
+        System.out.println("App Name is " + appName);
+        return "index.html";
+    }
+}

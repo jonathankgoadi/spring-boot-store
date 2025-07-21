f7192d32d053c78711cd96a77aa0b0b26582fe7c
Jonathan Kgoadi <jonathankgoadi@gmail.com>
2025-07-21 13:17:33 +0200

    define entities

--- src/main/java/com/johneycodes/store/entities/Profile.java
+++ src/main/java/com/johneycodes/store/entities/Profile.java
@@ -1,9 +1,8 @@
 package com.johneycodes.store.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;

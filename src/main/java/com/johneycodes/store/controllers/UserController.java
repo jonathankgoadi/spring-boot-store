1cd6f9e0c5e4955966e46ca85f2be508d4bbde4e
Jonathan Kgoadi <jonathankgoadi@gmail.com>
2025-07-25 23:41:28 +0200

    implement crud operations..create,update and delete

--- src/main/java/com/johneycodes/store/controllers/UserController.java
+++ src/main/java/com/johneycodes/store/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.johneycodes.store.controllers;
 
+import com.johneycodes.store.dtos.RegisterUserRequest;
+import com.johneycodes.store.dtos.UpdateUserRequest;
 import com.johneycodes.store.dtos.UserDto;
 import com.johneycodes.store.entities.User;
 import com.johneycodes.store.mappers.UserMapper;
@@ -8,6 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Iterator;
 import java.util.List;
@@ -41,5 +44,40 @@ public class UserController {
         }
 
         return ResponseEntity.ok(userMapper.toUserDto(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody RegisterUserRequest request, UriComponentsBuilder uriBuilder) {
+        var user = userMapper.toEntity(request);
+        user = userRepository.save(user);
+
+        var userDto = userMapper.toUserDto(user);
+        var uri = uriBuilder.path("/users/{id}").buildAndExpand(userDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(userDto);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @RequestBody UpdateUserRequest request) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        userMapper.updateUser(request, user);
+        user = userRepository.save(user);
+
+        return ResponseEntity.ok(userMapper.toUserDto(user));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        userRepository.delete(user);
+
+        return ResponseEntity.noContent().build();
+
     }
 }
--- src/main/java/com/johneycodes/store/mappers/UserMapper.java
+++ src/main/java/com/johneycodes/store/mappers/UserMapper.java
@@ -1,11 +1,16 @@
 package com.johneycodes.store.mappers;
 
+import com.johneycodes.store.dtos.RegisterUserRequest;
+import com.johneycodes.store.dtos.UpdateUserRequest;
 import com.johneycodes.store.dtos.UserDto;
 import com.johneycodes.store.entities.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toUserDto(User user);
+    User toEntity(RegisterUserRequest request);
+    void updateUser(UpdateUserRequest request, @MappingTarget User user);
 }

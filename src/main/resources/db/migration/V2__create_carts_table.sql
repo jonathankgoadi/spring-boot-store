09e1a070420081f88c0d4cc822f84c9ea8109233
Jonathan Kgoadi <jonathankgoadi@gmail.com>
2025-07-29 12:34:32 +0200

    cart  table migraton

---
+++ src/main/resources/db/migration/V2__create_carts_table.sql
@@ -0,0 +1,22 @@
+create table carts
+(
+    id           BINARY(16) default (uuid_to_bin(uuid())) primary key not null,
+    date_created DATE       default (CURDATE())           not null
+);
+
+create table cart_items
+(
+    id         bigint auto_increment
+        primary key,
+    cart_id    binary(16)    not null,
+    product_id bigint        not null,
+    quantity   int default 1 not null,
+    constraint cart_items_product_unique
+        unique (cart_id, product_id),
+    constraint cart_items_carts_id_fk
+        foreign key (cart_id) references carts (id)
+            on delete cascade,
+    constraint cart_items_products_id_fk
+        foreign key (product_id) references products (id)
+            on delete cascade
+);

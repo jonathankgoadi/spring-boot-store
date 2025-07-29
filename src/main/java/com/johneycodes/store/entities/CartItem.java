50b9c08e965306daf75c27267b1a71714a3524cc
Jonathan Kgoadi <jonathankgoadi@gmail.com>
2025-07-29 17:44:52 +0200

    cart entity classes

---
+++ src/main/java/com/johneycodes/store/entities/CartItem.java
@@ -0,0 +1,30 @@
+package com.johneycodes.store.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ColumnDefault("1")
+    @Column(name = "quantity")
+    private Integer quantity;
+
+}
--- src/main/java/com/johneycodes/store/entities/Category.java
+++ src/main/java/com/johneycodes/store/entities/Category.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Getter

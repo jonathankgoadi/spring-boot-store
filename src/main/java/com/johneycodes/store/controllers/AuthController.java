3758c3edaffbfb5e11e8cb7f4251056da4373e56
Jonathan Kgoadi <jonathankgoadi@gmail.com>
2025-08-18 23:54:50 +0200

    accessing the current user

--- src/main/java/com/johneycodes/store/controllers/AuthController.java
+++ src/main/java/com/johneycodes/store/controllers/AuthController.java
@@ -1,7 +1,10 @@
 package com.johneycodes.store.controllers;
 
 import com.johneycodes.store.dtos.JwtResponse;
+import com.johneycodes.store.dtos.UserDto;
 import com.johneycodes.store.dtos.UserLoginRequest;
+import com.johneycodes.store.mappers.UserMapper;
+import com.johneycodes.store.repositories.UserRepository;
 import com.johneycodes.store.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -10,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -18,6 +22,8 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
 
     @PostMapping("login")
@@ -40,6 +46,20 @@ public class AuthController {
         System.out.println("validate called");
         var token = authHeader.replace("Bearer ","");
         return jwtService.validateToken(token);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+
+        var user = userRepository.findByEmail(email).orElse(null);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return  ResponseEntity.ok(userMapper.toUserDto(user));
+
     }
 
     @ExceptionHandler(BadCredentialsException.class)

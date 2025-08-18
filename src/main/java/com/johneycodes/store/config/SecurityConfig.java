4650c3b0506e27221ca80634a4db0a26be2dfa1f
Jonathan Kgoadi <jonathankgoadi@gmail.com>
2025-08-18 22:52:40 +0200

    validating json tokens

--- src/main/java/com/johneycodes/store/config/SecurityConfig.java
+++ src/main/java/com/johneycodes/store/config/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(c -> c.requestMatchers("/carts/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/validate").permitAll()
                         .anyRequest().authenticated());
 
         return http.build();

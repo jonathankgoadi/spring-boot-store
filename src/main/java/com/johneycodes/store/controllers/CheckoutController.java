ce6bfc565121a286b3e83cd0df52b53beef9f0b2
Jonathan Kgoadi <jonathankgoadi@gmail.com>
2025-09-22 21:18:16 +0200

    implement checkout webhook

--- src/main/java/com/johneycodes/store/controllers/CheckoutController.java
+++ src/main/java/com/johneycodes/store/controllers/CheckoutController.java
@@ -3,28 +3,73 @@ package com.johneycodes.store.controllers;
 import com.johneycodes.store.dtos.CheckoutRequestDto;
 import com.johneycodes.store.dtos.CheckoutResponse;
 import com.johneycodes.store.dtos.ErrorDto;
+import com.johneycodes.store.entities.OrderStatus;
 import com.johneycodes.store.exceptions.CartEmptyException;
 import com.johneycodes.store.exceptions.CartNotFoundException;
 import com.johneycodes.store.exceptions.PaymentException;
+import com.johneycodes.store.repositories.OrderRepository;
 import com.johneycodes.store.services.CheckoutService;
+import com.stripe.exception.SignatureVerificationException;
 import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.net.Webhook;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/checkout")
 public class CheckoutController {
 
     private final CheckoutService checkoutService;
+    private final OrderRepository orderRepository;
 
-    @PostMapping("/checkout")
+    @Value("${stripe.webhookSecretKey}")
+    private  String webhookSecretKey;
+
+    @PostMapping
     public CheckoutResponse checkout(@RequestBody CheckoutRequestDto request) {
         return checkoutService.checkout(request);
+    }
+
+    @PostMapping("/webhook")
+    public ResponseEntity<Void> handleWebhook(
+            @RequestHeader("Stripe-Signature") String signature,
+           @RequestBody  String payload
+    ){
+        try {
+            var  event = Webhook.constructEvent(payload,signature, webhookSecretKey);
+            System.out.println("event type: " + event.getType());
+
+           var stripeObject = event.getDataObjectDeserializer().getObject().orElse(null);
+
+           switch (event.getType()) {
+               case "payment_intent.succeeded" -> {
+                   //Update  order status(paid)
+                   var paymentIntent = (PaymentIntent)  stripeObject;
+                   if (paymentIntent != null) {
+                   var orderId = paymentIntent.getMetadata().get("order_id");
+                   var order = orderRepository.findById(Long.valueOf(orderId)).orElseThrow();
+                   order.setStatus(OrderStatus.PAID);
+                   System.out.println("order status: " + order.getStatus());
+                   orderRepository.save(order);
+                   }
+               }
+               case "payment_intent.failed" -> {
+                   //update order status (failed)
+               }
+
+           }
+            return ResponseEntity.ok().build();
+
+        } catch (SignatureVerificationException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     @ExceptionHandler(PaymentException.class)
--- src/main/java/com/johneycodes/store/services/StripePaymentGateway.java
+++ src/main/java/com/johneycodes/store/services/StripePaymentGateway.java
@@ -1,6 +1,7 @@
 package com.johneycodes.store.services;
 
 import com.johneycodes.store.entities.Order;
+import com.johneycodes.store.entities.OrderItem;
 import com.johneycodes.store.exceptions.PaymentException;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
@@ -23,24 +24,12 @@ public class StripePaymentGateway implements PaymentGateway {
             var builder = SessionCreateParams.builder()
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
-                    .setCancelUrl(websiteUrl + "/checkout-cancel");
+                    .setCancelUrl(websiteUrl + "/checkout-cancel")
+                    .putMetadata("order_id", order.getId().toString());
 
             order.getOrderItems().forEach(item -> {
-                var lineItem = SessionCreateParams.LineItem.builder()
-                        .setQuantity(Long.valueOf(item.getQuantity()))
-                        .setPriceData(
-                                SessionCreateParams.LineItem.PriceData.builder()
-                                        .setCurrency("zar")
-                                        .setUnitAmountDecimal(item.getUnitPrice().multiply(BigDecimal.valueOf(100)))
-                                        .setProductData(
-                                                SessionCreateParams.LineItem.PriceData.ProductData.builder()
-                                                        .setName(item.getProduct().getName())
-                                                        .build()
-                                        ).build()
-                        ).build();
-
+                var lineItem = createLineItem(item);
                 builder.addLineItem(lineItem);
-
             });
 
             var session = Session.create( builder.build());
@@ -50,5 +39,26 @@ public class StripePaymentGateway implements PaymentGateway {
             System.out.println(e.getMessage());
             throw new PaymentException();
         }
+    }
+
+    private SessionCreateParams.LineItem createLineItem(OrderItem item) {
+        return SessionCreateParams.LineItem.builder()
+                .setQuantity(Long.valueOf(item.getQuantity()))
+                .setPriceData(createPriceData(item))
+                .build();
+    }
+
+    private SessionCreateParams.LineItem.PriceData createPriceData(OrderItem item) {
+        return SessionCreateParams.LineItem.PriceData.builder()
+                .setCurrency("zar")
+                .setUnitAmountDecimal(item.getUnitPrice().multiply(BigDecimal.valueOf(100)))
+                .setProductData(createProductData(item))
+                .build();
+    }
+
+    private SessionCreateParams.LineItem.PriceData.ProductData createProductData(OrderItem item) {
+        return SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                .setName(item.getProduct().getName())
+                .build();
     }
 }
